97c65b62b08db0ec57b1a7708ea82856bf330a14
Home <home@MacBook-Pro-Home.local>
2024-01-10 01:33:28 +0200

    commit 1

---
+++ script.js
@@ -0,0 +1,53 @@
+import fetch from "node-fetch";
+
+
+// const pricParm = process.argv.slice(2)
+// let cycleKey = "SSS-1111,SSS-2222"
+
+// const token = process.env.ACCESS_TOKEN
+// const testVar = process.env.TEST_VAR
+// const envKey = process.env.ENV_KEY
+// console.log(testVar)
+
+// if ( pricParm[0] != undefined ){
+//     // console.log(pricParm[0])
+//     cycleKey = pricParm[0].split('=')[1]
+// }
+
+console.log("~~~~~~~~~~~~")
+console.log("Hello")
+console.log("~~~~~~~~~~~~")
+console.log("?!?!?!?")
+console.log("~~~~~~~~~~~~")
+
+// let cycleKeys = cycleKey.split(",")
+// console.log(cycleKeys)
+
+// for (let i in cycleKeys){
+
+//     let data = {
+//         "ref":"master", 
+//         "inputs": 
+//             {
+//                 "cycle_key": `${cycleKeys[i]}`,
+//                 "env_key": `${envKey}`
+//             }
+//     }
+//     async function executionData() {
+//         const response = await fetch(`https://api.github.com/repos/Spirlhark/cycle_run/actions/workflows/79980694/dispatches`, {
+//             method: "POST",
+//             headers: {
+//                 Authorization: `token ${token}`,
+//                 Accept: "application/vnd.github.v3+json"
+//             },
+//             body: JSON.stringify(data)
+//             });
+            
+//         console.log("++++++++")
+//         console.log(response.status);
+//         console.log("++++++++")
+    
+//     }
+    
+//     executionData()
+// }
